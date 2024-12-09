ffe107a66ba55944127b5356bd296c6123525ebb
Carlos Cativo <cativo23.kt@gmail.com>
2024-12-09 01:06:31 -0600

    ♻️ refactor(hero): update layout and replace anchor with NuxtLink for project navigation

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -1,19 +1,18 @@
 <template>
   <section class="text-center py-5">
-    <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="hero-content flex-col lg:flex-row">
       <img src="/img/akira.jpeg" class="max-w-36 rounded-full shadow-2xl" alt="Foto de perfil" />
       <h2 class="text-4xl font-bold mb-4">Backend Developer & Tech Enthusiast</h2>
       <p class="text-xl mb-8">Crafting robust server-side solutions and sharing knowledge</p>
-      <a
-        class="bg-tokyo-night-red text-tokyo-night-dark px-6 py-2 rounded hover:bg-tokyo-night-cyan transition-colors duration-200 hover:cursor-pointer">
+      <NuxtLink :to="`/projects`"
+        class="btn bg-tokyo-night-red text-tokyo-night-dark px-6 py-2 rounded hover:bg-tokyo-night-cyan transition-colors duration-200 hover:cursor-pointer">
         View Projects
-      </a>
+      </NuxtLink>
     </div>
   </section>
 </template>
 
 <script lang="ts" setup>
-
 </script>
 
 <style></style>
