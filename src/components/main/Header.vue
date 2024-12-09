3d21f7aae49ae7b2f68fad17892ad94ac7b03224
Carlos Cativo <cativo23.kt@gmail.com>
2024-12-09 01:10:11 -0600

    ✨ feat(header): add NuxtLink to logo and implement mobile toggle menu

--- src/components/main/Header.vue
+++ src/components/main/Header.vue
@@ -1,32 +1,73 @@
 <template>
   <header class="navbar bg-tokyo-night-dark p-4 sticky top-0 z-10">
     <nav class="container mx-auto flex justify-between items-center">
-      <h1 class="text-2xl font-bold text-tokyo-night-cyan">
-        <VueTyping :sentences="sentences" :minTypeSpeed="settings.minTypeSpeed" :maxTypeSpeed="settings.maxTypeSpeed"
-          :eraseSpeed="settings.eraseSpeed" :eraseDelay="settings.eraseDelay" :writeDelay="settings.writeDelay"
-          :caret="settings.caret" :loop="settings.loop">
-          <template #before>
-            <span class="text-tokyo-night-purple">{</span>
-          </template>
-          <template #after>
-            <span class="text-tokyo-night-purple">}</span>
-          </template>
-        </VueTyping>
-      </h1>
-      <ul class="flex space-x-4">
-        <li v-for="item in navItems" :key="item.name">
+      <NuxtLink to="/" class="md:w-[250px]">
+        <h1 class="text-2xl font-bold text-tokyo-night-cyan">
+          <span class="block md:hidden">
+            <span class="text-tokyo-night-purple">{</span>CC<span class="text-tokyo-night-purple">}</span>
+          </span> <!-- Show on mobile devices -->
+          <span class="hidden md:block">
+            <VueTyping :sentences="sentences" :minTypeSpeed="settings.minTypeSpeed"
+              :maxTypeSpeed="settings.maxTypeSpeed" :eraseSpeed="settings.eraseSpeed" :eraseDelay="settings.eraseDelay"
+              :writeDelay="settings.writeDelay" :caret="settings.caret" :loop="settings.loop">
+              <template #before>
+                <span class="text-tokyo-night-purple">{</span>
+              </template>
+              <template #after>
+                <span class="text-tokyo-night-purple">}</span>
+              </template>
+            </VueTyping>
+          </span>
+        </h1>
+      </NuxtLink>
+      <div class="hidden md:flex space-x-4">
+        <ul class="flex space-x-4">
+          <li v-for="item in navItems" :key="item.name">
             <NuxtLink :href="item.link" :class="{
-            'text-tokyo-night-red font-bold': currentRoute.path === item.link,
-            'hover:text-tokyo-night-cyan transition-colors duration-200': currentRoute.path !== item.link
+              'text-tokyo-night-red font-bold': currentRoute.path === item.link,
+              'hover:text-tokyo-night-cyan transition-colors duration-200': currentRoute.path !== item.link
             }">{{ item.name }}</NuxtLink>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </div>
+      <button @click="isMenuOpen = !isMenuOpen" class="md:hidden">
+        <!-- Hamburger icon -->
+        <LucideMenu/>
+      </button>
     </nav>
+    <transition
+      enter-active-class="transition-transform transform duration-300"
+      enter-from-class="translate-x-full"
+      enter-to-class="translate-x-0"
+      leave-active-class="transition-transform transform duration-300"
+      leave-from-class="translate-x-0"
+      leave-to-class="translate-x-full"
+    >
+      <div v-if="isMenuOpen"
+        class="fixed inset-0 bg-tokyo-night-dark bg-opacity-90 z-20 flex flex-col items-center justify-center md:hidden">
+        <button @click="isMenuOpen = false" class="absolute top-4 right-4">
+          <!-- Close icon -->
+          <LucideX/>
+        </button>
+        <ul class="space-y-4">
+          <li v-for="item in navItems" :key="item.name">
+            <NuxtLink @click="isMenuOpen = false" :href="item.link" :class="{
+              'text-tokyo-night-red font-bold': currentRoute.path === item.link,
+              'hover:text-tokyo-night-cyan transition-colors duration-200': currentRoute.path !== item.link
+            }">
+              {{ item.name }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </transition>
   </header>
 </template>
 
 <script lang="ts" setup>
 import VueTyping from "@dmncodes/vue-typing";
+import { ref, reactive } from 'vue';
+import { useRoute } from 'vue-router';
 
 // Define reactive properties for the typing settings
 const settings = reactive({
@@ -39,7 +80,9 @@ const settings = reactive({
   loop: true,
 });
 
-//Navigation items
+const isMenuOpen = ref(false);
+
+// Navigation items
 const navItems = ref([
   { name: 'Home', link: '/' },
   { name: 'About', link: '/about' },
@@ -57,20 +100,3 @@ const sentences = [
   ' cativo23'
 ];
 </script>
-
-<style>
-.dmn-typing .caret {
-  color: inherit;
-  animation: blink 1s infinite;
-}
-
-.dmn-typing .typing {
-  animation: none;
-}
-
-@keyframes blink {
-  50% {
-    opacity: 0;
-  }
-}
-</style>
