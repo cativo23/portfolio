ae51ecdcb8df46589b885d04b288925b89df8ce6
Carlos Cativo <cativo23.kt@gmail.com>
2025-06-11 23:17:29 -0600

    💄 feat(Contact, BlogSection, [...slug], contact): enhance UI and improve form accessibility

--- src/pages/blog/[...slug].vue
+++ src/pages/blog/[...slug].vue
@@ -1,10 +1,10 @@
 <template>
   <main class="prose prose-lg mx-auto py-2 px-2 sm:p-6 lg:p-8 max-w-full sm:max-w-3xl lg:max-w-full text-justify">
     <NuxtLink to="/blog" class="btn flex items-center mb-4 text-tokyo-night-highlight hover:text-tokyo-night-secondary">
-      <LucideArrowLeft class="w-4 h-4 ml-2"/>Back to Blog
+      <LucideArrowLeft class="w-4 h-4 ml-2" />Back to Blog
     </NuxtLink>
-    <h1 v-if="data" class="text-3xl text-secondary">{{ data.title }}</h1>
-    <ContentDoc v-if="data" :value="data" class="prose-tokyo"/>
+    <h1 v-if="data" class="text-3xl font-bold text-tokyo-night-highlight font-mono">{{ data.title }}</h1>
+    <ContentDoc v-if="data" :value="data" class="prose-tokyo" />
   </main>
 </template>
 
@@ -32,5 +32,4 @@ if (!data.value) {
 
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
