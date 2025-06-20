3e17c66b5880e4b0670e0abf93369efe2be2a776
Carlos Cativo <cativo23.kt@gmail.com>
2025-06-19 21:11:46 -0600

    ✨ style(Hero): enhance layout structure for hero section

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="text-center py-5">
-    <div class="hero-content flex-col lg:flex-row">
-      <img src="/img/akira.jpeg" class="max-w-36 rounded-full shadow-2xl" alt="Foto de perfil" />
+    <div class="hero-content flex-col lg:flex-row items-center justify-center">
+      <div class="flex justify-center items-center w-full mb-4 lg:mb-0">
+        <img src="/img/akira.jpeg" class="max-w-36 rounded-full shadow-2xl" alt="Foto de perfil" />
+      </div>
       <h2 class="text-4xl font-bold mb-4 text-info">Backend Developer & Tech Enthusiast</h2>
       <p class="text-xl mb-8">Crafting robust server-side solutions and sharing knowledge</p>
       <NuxtLink :to="`/projects`"
